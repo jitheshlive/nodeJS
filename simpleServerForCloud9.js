8fee996b03c631bea19fb8b3e802afc98e51abfe
Jithesh Gopinathan <jitheshlive@gmail.com>
2016-04-20 16:38:11 +0530

    Update simpleServerForCloud9.js

--- simpleServerForCloud9.js
+++ simpleServerForCloud9.js
@@ -10,9 +10,8 @@ var server = http.createServer(function(request,response){
 
 });
 
-
-
 server.listen(process.env.PORT, process.env.IP);
-
+//If using in local comment the above line and uncomment the line below
+//server.listen(8000);
 
 console.log("Server running");
